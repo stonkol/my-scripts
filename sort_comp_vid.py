01335c9be1eb183fd921b80aed1f085dcf7f05a7
stonkol <hector96fi@yahoo.es>
2024-02-11 13:27:16 +0100

    Update sort_comp_vid.py

--- sort_comp_vid.py
+++ sort_comp_vid.py
@@ -25,29 +25,31 @@ def move_and_compress_videos(source_dir):
             destination_path = os.path.join(destination_dir, file_name)
             shutil.move(source_path, destination_path)
 
-    print(f"[{total_videos}] videos moved 🚚 -to-> 'vids' folder.")
+    print(f"   [{total_videos}] videos moved 🚚 -to-> 'vids' folder.")
 
     # Compress videos in the 'vids' folder and save in 'comp vids' folder
     for file_name in os.listdir(destination_dir):
         if file_name.lower() == ".ds_store":
             continue  # Skip .DS_Store files
         source_path = os.path.join(destination_dir, file_name)
-        compressed_path = os.path.join(compressed_dir, f"{file_name}")
+        compressed_path = os.path.join(compressed_dir, file_name)
 
         # Using ffmpeg to compress videos
         output_file_name = f"{file_name}"  # New file name for compressed video
         output_path = os.path.join(compressed_dir, output_file_name)
         command = ["ffmpeg", "-i", source_path, "-c:v", "libx264", "-crf", "23", "-c:a", "aac", output_path]
-        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True) as p:
-            for line in tqdm(p.stdout, total=100, desc=f"Compressing {file_name}", unit=" frames"):
-                pass
+        
         compressed_videos += 1
-        tqdm.write(f"[{compressed_videos}/{total_videos}] {file_name} compressed")
+        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True) as p:
+            for line in tqdm(p.stdout, total=100, desc=f"[{compressed_videos}/{total_videos}] Compressing {file_name}", unit=" frames"):
+                pass
+        
+#        tqdm.write(f"[{compressed_videos}/{total_videos}] {file_name} compressed")
         # replace last line when video is compressed
 #        tqdm.write(f"[{compressed_videos}/{total_videos}] {file_name} compressed", end="\r")
 
 
-    print("\n💈 All Videos Compressed 💈")
+    print("\n      💈All Videos Compressed💈")
 
 if __name__ == "__main__":
     source_directory = input("    💬 Path of your folder -> ")
